2489f2a10eac8bf462d4a878f43ecacf55be1501
Simone <simone.manenti@liparipeople.com>
2023-10-13 16:08:00 +0200

    Add static buttons to add new bank account

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BankAccountContainer from './components/bank-accounts-container/bank-accounts-container.component';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Our first app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <BankAccountContainer></BankAccountContainer>
+            {/* <Text>Our first app!</Text>
+      <StatusBar style="auto" /> */}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        width: '100%',
+        padding: '0em 1em'
+        // flex: 1,
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center'
+    }
 });
