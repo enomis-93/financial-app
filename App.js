c6939aa0e32b976516cc6e73b7c80e487dc41886
DESKTOP-FR872JU\Marco Di Gifco <marco.digifico@liparipeople.com>
2023-10-13 16:47:02 +0200

    upgrade merge header bar branch

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Dimensions } from "react-native";
 import ChartComponent from './components/chartComponent';
 import BankAccountContainer from './components/bank-accounts-container/bank-accounts-container.component';
+import ResponsiveAppBar from './components/header/responsive-app-bar.component';
 const screenWidth = Dimensions.get("window").width;
 export default function App() {
   return (
@@ -19,10 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        padding: '0em 1em'
-        // flex: 1,
-        // backgroundColor: '#fff',
-        // alignItems: 'center',
-        // justifyContent: 'center'
+        
     }
 });
