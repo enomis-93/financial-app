8fa24c15e759b8f19649056710751ae6803130b9
Simone <simone.manenti@liparipeople.com>
2023-10-23 16:08:09 +0200

    Form init

--- components/bank-accounts-container/add-bank-account-dialog/add-bank-account-dialog.tsx
+++ components/bank-accounts-container/add-bank-account-dialog/add-bank-account-dialog.tsx
@@ -49,6 +49,17 @@ export default function AddBankAccountDialog(props: AddBankAccountDialogProps) {
     const [excludeFromStats, setExcludeFromStats] = React.useState(false);
     const [archive, setArchive] = React.useState(false);
 
+    const [formData, setFormData] = useState({
+        name: { id: 1, value: '', type: 'string' },
+        currency: { id: 2, value: '', type: 'string' },
+        balance: { id: 3, value: '', type: 'string' },
+        color: { id: 4, value: '', type: 'string' },
+        type: { id: 5, value: '', type: 'string' }
+    });
+    const [name, setName] = React.useState('');
+    const [saldo, setSaldo] = React.useState(0);
+    const [valuta, setValuta] = React.useState('');
+
     const handleExcludeFromStatsChange = (event) => {
         setExcludeFromStats(event.target.checked);
     };
@@ -60,10 +71,6 @@ export default function AddBankAccountDialog(props: AddBankAccountDialogProps) {
     const handleClose = () => {
         onClose(false);
     };
-
-    const [name, setName] = React.useState('');
-    const [saldo, setSaldo] = React.useState(0);
-    const [valuta, setValuta] = React.useState('');
 
     const handleOnchangeName = (event) => {
         setName(event.target.value);
@@ -79,17 +86,9 @@ export default function AddBankAccountDialog(props: AddBankAccountDialogProps) {
         setValuta(event.target.value);
     };
 
-    const saveAccount = () => {
-        props.backAccounts.push({
-            id: props.backAccounts.length,
-            name: name,
-            color: 'none',
-            tipo: 'portafoglio',
-            saldo: saldo,
-            valuta: valuta
-        });
-        handleClose();
-        return;
+    const saveAccount = (event: Event) => {
+        event.preventDefault();
+        console.log(formData);
     };
 
     return (
@@ -116,108 +115,117 @@ export default function AddBankAccountDialog(props: AddBankAccountDialogProps) {
             <Divider />
             <DialogContent>
                 <Container>
-                    <div
-                        style={{
-                            display: 'flex',
-                            gap: '5px',
-                            marginBottom: '0.5em'
-                        }}
-                    >
-                        <TextField
-                            required
-                            id='outlined-required'
-                            value={name}
-                            onChange={handleOnchangeName}
-                            label='Name'
-                        />
-                        <ColorPickerInput />
-                    </div>
-                    <div
-                        style={{
-                            display: 'flex',
-                            gap: '5px',
-                            marginBottom: '0.5em'
-                        }}
-                    >
-                        <BankAccountInput></BankAccountInput>
-                    </div>
-                    <div
-                        style={{
-                            display: 'flex',
-                            gap: '5px',
-                            marginBottom: '0.5em'
-                        }}
-                    >
-                        <NumberInput
-                            style={{ width: '50%' }}
-                            value={saldo}
-                            onChange={handleOnchangeSaldo}
-                            handleOnchangeSaldo={handleOnchangeSaldo}
-                        ></NumberInput>
-                        <TextField
-                            style={{ width: '50%' }}
-                            id='outlined-select-currency'
-                            select
-                            label='Valuta'
-                            defaultValue='EUR'
-                            value={valuta}
-                            onChange={handleOnchangeValuta}
+                    <form onSubmit={saveAccount}>
+                        <div
+                            style={{
+                                display: 'flex',
+                                gap: '5px',
+                                marginBottom: '0.5em'
+                            }}
                         >
-                            {currencies.map((option) => (
-                                <MenuItem
-                                    key={option.value}
-                                    value={option.value}
-                                >
-                                    {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </div>
+                            <TextField
+                                required
+                                id='outlined-required'
+                                value={formData.name}
+                                onChange={handleOnchangeName}
+                                label='Name'
+                            />
+                            <ColorPickerInput />
+                        </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                gap: '5px',
+                                marginBottom: '0.5em'
+                            }}
+                        >
+                            <BankAccountInput></BankAccountInput>
+                        </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                gap: '5px',
+                                marginBottom: '0.5em'
+                            }}
+                        >
+                            <NumberInput
+                                style={{ width: '50%' }}
+                                value={saldo}
+                                onChange={handleOnchangeSaldo}
+                                handleOnchangeSaldo={handleOnchangeSaldo}
+                            ></NumberInput>
+                            <TextField
+                                style={{ width: '50%' }}
+                                id='outlined-select-currency'
+                                select
+                                label='Valuta'
+                                defaultValue='EUR'
+                                value={valuta}
+                                onChange={handleOnchangeValuta}
+                            >
+                                {currencies.map((option) => (
+                                    <MenuItem
+                                        key={option.value}
+                                        value={option.value}
+                                    >
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        </div>
 
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            gap: '5px',
-                            marginBottom: '0.5em'
-                        }}
-                    >
-                        <FormControlLabel
-                            control={
-                                <Switch
-                                    checked={excludeFromStats}
-                                    onChange={handleExcludeFromStatsChange}
-                                    name='excludeFromStats'
-                                />
-                            }
-                            label='Escludi dalle statistiche'
-                        />
-                        <FormControlLabel
-                            control={
-                                <Switch
-                                    checked={archive}
-                                    onChange={handleArchiveChange}
-                                    name='archive'
-                                />
-                            }
-                            label='Archivia'
-                        />
-                    </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: '5px',
+                                marginBottom: '0.5em'
+                            }}
+                        >
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        checked={excludeFromStats}
+                                        onChange={handleExcludeFromStatsChange}
+                                        name='excludeFromStats'
+                                    />
+                                }
+                                label='Escludi dalle statistiche'
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        checked={archive}
+                                        onChange={handleArchiveChange}
+                                        name='archive'
+                                    />
+                                }
+                                label='Archivia'
+                            />
+                        </div>
+                        <div
+                            style={{
+                                padding: '1em',
+                                display: 'flex',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Button
+                                type='submit'
+                                variant='contained'
+                                color='success'
+                                style={{
+                                    borderRadius: '20px',
+                                    padding: '0.5em 3em'
+                                }}
+                            >
+                                Save
+                            </Button>
+                        </div>
+                    </form>
                 </Container>
             </DialogContent>
-            <DialogActions
-                sx={{ p: 1 }}
-                style={{ display: 'flex', justifyContent: 'center' }}
-            >
-                <Button
-                    onClick={saveAccount}
-                    variant='contained'
-                    color='success'
-                    style={{ borderRadius: '20px', padding: '0.5em 3em' }}
-                >
-                    Save
-                </Button>
-            </DialogActions>
+            {/* <DialogActions sx={{ p: 1 }}></DialogActions> */}
         </Dialog>
     );
 }
